79a3f9fb289437c82f39fcac901156a055b92ed6
Francois Lebel <francoislebel@gmail.com>
2011-02-21 18:16:56 -0500

    Added the --move-twin-files option to move the files that are prefixed by the JPEG's filename without its extension, ie. IMG_0001.JPG and IMG_0001.CR2.

--- move_jpegs_in_folders_by_exif_date.py
+++ move_jpegs_in_folders_by_exif_date.py
@@ -20,7 +20,9 @@
 #   A script to move JPEG files in directories ('yyyy-mm-dd') according
 #   to their Exif.Image.DateTime EXIF tag. The directory given as the
 #   positional argument is not searched recursively and will become the
-#   parent directory for the created directories.
+#   parent directory for the created directories. The --move-twin-files
+#   option can be used to move non-JPEG files that have the same basename
+#   as the JPEG file but differs in extension, such as RAW files.
 #
 #   Dependencies:
 #     pyexiv2 0.2.2 http://tilloy.net/dev/pyexiv2/
@@ -36,6 +38,7 @@ JPEG_EXTENSIONS = ('JPG', 'JPEG')
 
 def main():
     parser = OptionParser(usage='Usage: %prog path_to_directory')
+    parser.add_option('--move-twin-files', action='store_true', default=True, dest='move_twin_files', help="also move the files that are prefixed by the JPEG's filename without its extension, ie. IMG_0001.JPG and IMG_0001.CR2")
     parser.add_option('--dry-run', action='store_true', default=False, dest='dry_run', help='display the actions that will be undertaken without actually executing them')
     (options, args) = parser.parse_args()
     if len(args) != 1:
@@ -45,9 +48,9 @@ def main():
         parser.error("the files' directory specified is invalid.")
     # Get the absolute path and normalize its case
     directory = os.path.realpath(os.path.normcase(directory))
-    run(directory, options.dry_run)
+    run(directory, options.move_twin_files, options.dry_run)
 
-def run(directory, dry_run):
+def run(directory, move_twin_files, dry_run):
     files = []
     for extension in JPEG_EXTENSIONS:
         file_glob = "%s*.%s" % (os.path.join(directory, '*'), extension)
@@ -72,15 +75,35 @@ def run(directory, dry_run):
                 print '[dry-run]', directory_msg,
             print 'Done.'
         file_name = os.path.basename(os.path.join(date_directory_path, f))
-        moved_file_path = os.path.join(date_directory_path, file_name)
         # Move the photo into the directory
         moving_msg = "Moving file '%s' into '%s'..." % (file_name, date_directory_path)
         if not dry_run:
             print moving_msg,
+            moved_file_path = os.path.join(date_directory_path, file_name)
             shutil.move(f, moved_file_path)
         else:
             print '[dry-run]', moving_msg,
         print 'Done.'
+        # Move the files that are prefixed by the filename without its
+        # extension, ie. IMG_0001.JPG and IMG_0001.CR2
+        if move_twin_files:
+            basename = os.path.splitext(f)[0]
+            twin_files_glob = "%s.*" % basename
+            twin_files = glob.glob(twin_files_glob)
+            # Remove the current file from the list if it hasn't been moved
+            # as no action is being executed
+            if dry_run:
+                twin_files.remove(f)
+            for tf in twin_files:
+                twin_file_name = os.path.basename(os.path.join(date_directory_path, tf))
+                twin_moving_msg = "Moving file '%s' into '%s'..." % (twin_file_name, date_directory_path)
+                if not dry_run:
+                    print twin_moving_msg,
+                    twin_moved_file_path = os.path.join(date_directory_path, twin_file_name)
+                    shutil.move(tf, twin_moved_file_path)
+                else:
+                    print '[dry-run]', twin_moving_msg,
+                print 'Done.'
 
 if __name__ == "__main__":
     main()
